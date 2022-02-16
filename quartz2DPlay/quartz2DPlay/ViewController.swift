c33a314afa41464cda6bb5fc59173edb88045597
Lei Zhao <lei.zhao@coupa.com>
2022-02-16 07:00:14 -0800

    add by auto checkIn

--- quartz2DPlay/quartz2DPlay/ViewController.swift
+++ quartz2DPlay/quartz2DPlay/ViewController.swift
@@ -11,34 +11,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private  var imageView: UIImageView? {
-        let imageView = UIImageView()
-        imageView.center = view.center
-        let width: CGFloat = 300
-        let height: CGFloat = 200
-        imageView.bounds.size = CGSize(width: width, height: height)
-
-//        let img = UIImage(named: "me")?.circleImage(withBorderWidth: 5, andBorderColor: .systemPink)
-        let img = UIImage(named: "me")?.circleImage()
-        imageView.image = img
-        imageView.contentMode = .scaleAspectFit
-
-        // save image to sandbox
-        if let data = img?.pngData() {
-            let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first! + "xx.png"
-            let url = URL(fileURLWithPath: path)
-            try? data.write(to: url)
-        }
-
-
-        return imageView
-    }
+    private var imageView: UIImageView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.addSubview(imageView!)
-        view.backgroundColor = .systemCyan
+        setImageView()
+//        view.addSubview(imageView!)
+//        view.backgroundColor = .systemCyan
 //        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
 //        let ctx = UIGraphicsGetCurrentContext()
 //        if let ctx = ctx {
@@ -51,6 +31,34 @@ class ViewController: UIViewController {
 //        if let screenShot = screenShot {
 //            UIImageWriteToSavedPhotosAlbum(screenShot, self, #selector(saveComplete), nil)
 //        }
+    }
+
+    private func setImageView() {
+        let imageView = UIImageView()
+        imageView.center = view.center
+        let width: CGFloat = 300
+        let height: CGFloat = 200
+        imageView.bounds.size = CGSize(width: width, height: height)
+        imageView.backgroundColor = .systemCyan
+
+//        let img = UIImage(named: "me")?.circleImage(withBorderWidth: 5, andBorderColor: .systemPink)
+        let img = UIImage(named: "me")?.circleImage()
+        imageView.image = img
+        imageView.contentMode = .scaleAspectFit
+
+//        // save image to sandbox
+//        if let data = img?.pngData() {
+//            let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first! + "xx.png"
+//            let url = URL(fileURLWithPath: path)
+//            try? data.write(to: url)
+//        }
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
+        imageView.addGestureRecognizer(tapGesture)
+        imageView.isUserInteractionEnabled = true
+
+        view.addSubview(imageView)
+        self.imageView = imageView
     }
 
     @objc private func saveComplete(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafeRawPointer) {
@@ -66,5 +74,17 @@ class ViewController: UIViewController {
         let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
         alertController.addAction(alertAction)
         present(alertController, animated: true, completion: nil)
+    }
+
+    @objc
+    private func tap(tapGesture: UIGestureRecognizer) {
+        print(imageView)
+        let tapPoint = tapGesture.location(in: imageView!)
+        print(tapPoint)
+        if tapPoint.x < imageView!.bounds.width*0.5 {
+            print("tap left")
+        } else {
+            print("tap right")
+        }
     }
 }
