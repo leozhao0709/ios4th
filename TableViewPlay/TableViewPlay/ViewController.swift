3f274c097417f8f381313c41058be96d7c715f9d
Lei Zhao <lei.zhao@coupa.com>
2022-02-21 18:00:15 -0800

    add by auto checkIn

--- TableViewPlay/TableViewPlay/ViewController.swift
+++ TableViewPlay/TableViewPlay/ViewController.swift
@@ -11,18 +11,36 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
 //    @IBOutlet weak var tableView: UITableView!
 
-    private let tableView: UITableView = {
+    private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(PlayTableViewCell.self, forCellReuseIdentifier: PlayTableViewCell.identifier)
         return tableView
     }()
+
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshTable), for: .valueChanged)
+        refreshControl.tintColor = .red
+        refreshControl.attributedTitle = NSAttributedString("拼命加载中。。。")
+        return refreshControl
+    }()
+
+    @objc private func refreshTable() {
+        print("....start refresh....")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            print("....end refresh....")
+            self.refreshControl.endRefreshing()
+        }
+    }
 
     private lazy var blogModels: [BlogModel] = {
         // load data
         guard let path = Bundle.main.path(forResource: "weibos.plist", ofType: nil) else {
             return []
         }
-        guard let tempArray = NSArray(contentsOfFile: path) else {return []}
+        guard let tempArray = NSArray(contentsOfFile: path) else {
+            return []
+        }
         return tempArray.map { dict -> BlogModel in
             BlogModel(dict: dict as! [String: Any])
         }
@@ -34,14 +52,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.backgroundColor = .link
+//        tableView.backgroundColor = .link
 
         view.addSubview(tableView)
+
+        tableView.addSubview(refreshControl)
 
 //        // automatically calculate row height. Best work with auto layout
 //        tableView.rowHeight = UITableView.automaticDimension;
 //        // set estimatedRowHeight to whatever is the fallBack rowHeight
 //        tableView.estimatedRowHeight = 44;
+
     }
 
     override func viewDidLayoutSubviews() {
@@ -56,7 +77,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: PlayTableViewCell = tableView.dequeueReusableCell(withIdentifier: PlayTableViewCell.identifier, for: indexPath) as! PlayTableViewCell
         cell.blogModel = blogModels[indexPath.row]
-        print(indexPath.row)
         return cell
     }
 
