8a8f9e2d7b113c3c4dda8b9ce390ce8951d48730
Lei Zhao <lei.zhao@coupa.com>
2022-02-27 09:40:30 -0800

    add by auto checkIn

--- WaterFallLayout/WaterFallLayout/ViewController.swift
+++ WaterFallLayout/WaterFallLayout/ViewController.swift
@@ -1,39 +1,60 @@
-//
-//  ViewController.swift
-//  WaterFallLayout
-//
-//  Created by Lei Zhao on 2/25/22.
-//
-//
-
 import UIKit
 
 
 class ViewController: UIViewController {
+
+    private weak var refreshControl: UIRefreshControl?
+    private weak var collectionView: UICollectionView?
+
+    var itemCount = 10
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
         setupCollectionView()
+        setupRefreshControl()
     }
 
     private func setupCollectionView() {
-        let layout = WaterFallLayout()
-        layout.minimumLineSpacing = 20
-        layout.minimumInteritemSpacing = 20
+        let layout = WaterFallLayout(columnCount: 3, interItemSpacing: 10, lineSpacing: 10)
+        layout.sectionInset = UIEdgeInsets(top: 50, left: 10, bottom: 100, right: 10)
+        layout.delegate = self
 
         let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
         collectionView.dataSource = self
+        collectionView.backgroundColor = .tintColor
 
         collectionView.register(WaterFallCell.self, forCellWithReuseIdentifier: WaterFallCell.identifier)
         view.addSubview(collectionView)
+        self.collectionView = collectionView
+    }
+
+    private func setupRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        self.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        collectionView?.refreshControl = refreshControl
+    }
+
+    @objc private func refresh() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [self] in
+            itemCount += 10
+            refreshControl?.endRefreshing()
+            collectionView?.reloadData()
+        }
+    }
+}
+
+extension ViewController: WaterFallLayoutDelegate {
+    func heightForItem(at indexPath: IndexPath, itemPosition position: CGPoint, itemWidth width: CGFloat) -> CGFloat {
+        100 + CGFloat(indexPath.row % 7) * 10
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        100
+        itemCount
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
--- WaterFallLayout/WaterFallLayout/WaterFallLayout.swift
+++ WaterFallLayout/WaterFallLayout/WaterFallLayout.swift
@@ -4,14 +4,37 @@
 
 import UIKit
 
+protocol WaterFallLayoutDelegate {
+    func heightForItem(at indexPath: IndexPath, itemPosition position: CGPoint, itemWidth width: CGFloat) -> CGFloat
+}
+
 class WaterFallLayout: UICollectionViewFlowLayout {
     // our waterFallLayout item has same width, but different height
+    // default value if outsider not defined
     var columnCount = 3
     var interItemSpacing: CGFloat = 20
     var lineSpacing: CGFloat = 20
 
+
+    var delegate: WaterFallLayoutDelegate?
+
     private var columnHeightArr: [CGFloat] = []
     private var attrArr: [UICollectionViewLayoutAttributes]? = []
+
+    override init() {
+        super.init()
+    }
+
+    init(columnCount: Int, interItemSpacing: CGFloat, lineSpacing: CGFloat) {
+        super.init()
+        self.columnCount = columnCount
+        self.interItemSpacing = interItemSpacing
+        self.lineSpacing = lineSpacing
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     override func prepare() {
         super.prepare()
@@ -22,7 +45,7 @@ class WaterFallLayout: UICollectionViewFlowLayout {
         // only 1 section
         let itemsCount = collectionView!.numberOfItems(inSection: 0)
 
-        for i in 0 ..< itemsCount {
+        for i in 0..<itemsCount {
             let indexPath = IndexPath(item: i, section: 0)
             attrArr!.append(layoutAttributesForItem(at: indexPath)!)
         }
@@ -30,36 +53,37 @@ class WaterFallLayout: UICollectionViewFlowLayout {
     }
 
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-//        let attrs = super.layoutAttributesForElements(in: rect)
-//        guard let attrs = attrs else { return nil }
-//
-//        return attrs.map { attributes in
-//            let indexPath = attributes.indexPath
-//            return layoutAttributesForItem(at: indexPath)!
-//        }
-
         attrArr
     }
 
+
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        let attributes = super.layoutAttributesForItem(at: indexPath)
+        let attributes = super.layoutAttributesForItem(at: indexPath)?.copy() as! UICollectionViewLayoutAttributes
 
         let minColumnHeight = columnHeightArr.min()!
         let minColumnHeightIndex = columnHeightArr.firstIndex(of: minColumnHeight)!
 
-        let width = (collectionView!.bounds.width - CGFloat(columnCount - 1) * interItemSpacing) / CGFloat(columnCount);
-        let height = 100 + CGFloat.random(in: 0..<100)
-        let x: CGFloat = CGFloat(minColumnHeightIndex) * (interItemSpacing + width);
-        let y: CGFloat = minColumnHeight + lineSpacing;
+        let width = (collectionView!.bounds.width - sectionInset.left - sectionInset.right - CGFloat(columnCount - 1) * interItemSpacing) / CGFloat(columnCount);
+        let x: CGFloat = CGFloat(minColumnHeightIndex) * (interItemSpacing + width) + sectionInset.left;
+        var y: CGFloat = minColumnHeight + lineSpacing;
+        if minColumnHeight == 0 {
+            // first row, need section top but without line spacing
+            y += sectionInset.top - lineSpacing
+        }
 
-        attributes?.frame = CGRect(x: x, y: y, width: width, height: height)
+        var height = 100 + CGFloat(indexPath.row % 10) * 10
+        if let delegate = delegate {
+            height = delegate.heightForItem(at: indexPath, itemPosition: CGPoint(x: x, y: y), itemWidth: width)
+        }
 
-        columnHeightArr[minColumnHeightIndex] = attributes!.frame.maxY
+        attributes.frame = CGRect(x: x, y: y, width: width, height: height)
+
+        columnHeightArr[minColumnHeightIndex] = attributes.frame.maxY
 
         return attributes
     }
 
     override var collectionViewContentSize: CGSize {
-        CGSize(width: collectionView!.bounds.width, height: columnHeightArr.max()!)
+        CGSize(width: collectionView!.bounds.width, height: columnHeightArr.max()! + sectionInset.bottom)
     }
 }
