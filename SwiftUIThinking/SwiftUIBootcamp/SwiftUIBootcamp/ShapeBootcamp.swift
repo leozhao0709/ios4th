bc7e714dfe1b5dd3dd8ffe0e8f127745ccc9fb77
leozhao0709 <leo.zhao.real@gmail.com>
2024-02-06 23:09:03 -0800

    add by auto checkIn

--- SwiftUIThinking/SwiftUIBootcamp/SwiftUIBootcamp/ShapeBootcamp.swift
+++ SwiftUIThinking/SwiftUIBootcamp/SwiftUIBootcamp/ShapeBootcamp.swift
@@ -7,26 +7,33 @@
 
 import SwiftUI
 
+@available(iOS 17.0, *)
 struct ShapeBootcamp: View {
     
-    
     var body: some View {
-        let fillColor = Color(#colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 0.9339559189))
-        Circle()
-            .strokeBorder(.green, lineWidth: 20)
-            .background(Circle().fill(fillColor))
-//            .fill(.green)
-//            .fill(.green)
-//            .stroke()
-//            .stroke(.green)
-//            .stroke(.blue, lineWidth: 30)
-//            .stroke(.blue, style: StrokeStyle(lineWidth: 30, lineCap: .butt, dash: [10]))
-//            .trim(from: 0.4, to: 1)
+        let fillColor = Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
+        VStack{
+            Circle()
+            // .strokeBorder(.green, lineWidth: 20)
+            // .background(Circle().fill(fillColor))
+                .fill(Color.pink)
+                .stroke(fillColor)
+            //            .stroke()
+            //            .stroke(.green)
+            //            .stroke(.blue, lineWidth: 30)
+            //            .stroke(.blue, style: StrokeStyle(lineWidth: 30, lineCap: .butt, dash: [10]))
+            //            .trim(from: 0.4, to: 1)
+        }
+        
     }
 }
 
 struct ShapeBootcamp_Previews: PreviewProvider {
     static var previews: some View {
-        ShapeBootcamp()
+        if #available(iOS 17.0, *) {
+            ShapeBootcamp()
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
